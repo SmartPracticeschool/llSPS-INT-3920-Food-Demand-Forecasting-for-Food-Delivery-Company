c7e1285bc851a8fbd92523acff4bc1496328a9e1
suryatejpnsv <67155625+suryatejpnsv@users.noreply.github.com>
2020-09-29 15:42:11 +0530

    Add files via upload

--- ML-Flask/pyth.py
+++ ML-Flask/pyth.py
@@ -1,1 +1,33 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+model = pickle.load(open('random.pkl','rb'))
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    float_features = []
+    float_features.append(float(request.form['do']))
+    float_features.append(float(request.form['ph']))
+    float_features.append(float(request.form['co']))
+    float_features.append(float(request.form['bod']))
+    float_features.append(float(request.form['na']))
+    float_features.append(float(request.form['tc']))
+    float_features.append(float(request.form['tc1']))
+
+    final_features = [np.array(float_features)]
+    prediction = model.predict(final_features)
+
+    output = np.round_(prediction[[0]], 2)
+
+    return render_template('index.html', wqi=output[0])
+
+if __name__ == "__main__":
+    app.run(debug=False)
